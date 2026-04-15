36b472c6970672c55bbf331a0b049783c8743ef5
Maged Elsayed <magedelhossary663@gmail.com>
2026-04-15 22:27:14 +0400

    restore latest code changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config();
+// ❌ لا نستخدم dotenv في Render
+// import dotenv from "dotenv";
+// dotenv.config();
 
 import express from "express";
 import cors from "cors";
